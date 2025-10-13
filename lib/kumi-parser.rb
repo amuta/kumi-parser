3d487c53424a51397975245fdd3395ef9b663da6
Andre Muta <andremuta@gmail.com>
2025-10-13 18:37:02 -0300

    Tell Zeitwerk to ignore token_constants.rb
    File contains module-level constants, not a TokenConstants class. Manually require it instead of autoloading.

--- lib/kumi-parser.rb
+++ lib/kumi-parser.rb
@@ -8,9 +8,11 @@ require 'parslet'
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.ignore("#{__dir__}/kumi-parser.rb")
 loader.ignore("#{__dir__}/kumi/parser/version.rb")
+loader.ignore("#{__dir__}/kumi/parser/token_constants.rb")
 loader.setup
 
 require_relative 'kumi/parser/version'
+require_relative 'kumi/parser/token_constants'
 
 module Kumi
   module Parser
